c23676d8378470167775ce810e35f93ba33f7514
Md Somrat Akbor <somrat876@gmail.com>
2022-11-25 16:21:48 +0600

    done

--- MobileBillCalculate/Program.cs
+++ MobileBillCalculate/Program.cs
@@ -8,27 +8,24 @@ namespace MobileBillCalculate
 
         static void Main(string[] args)
         {
-            var peekHourStart = Convert.ToDateTime ("9:00:00 am") ;
-            var peekHourEnd = Convert.ToDateTime("10:59:59 pm"); ;
-   
-
-            var inputTime = "2019-08-31 08:59:13 am";
-            var outputTime = "2019-08-31 09:00:39 am";
+            //DateTime peekHourStart = Convert.ToDateTime ("9:00:00 am") ;
+            //DateTime peekHourEnd = Convert.ToDateTime("10:59:59 pm"); ;
             decimal bill = 0;
-
-            DateTime startTime = Convert.ToDateTime(inputTime);
-            DateTime endTime = Convert.ToDateTime(outputTime);
+             
+            DateTime startTime = Convert.ToDateTime("2019-08-31 08:59:13 am");
+            DateTime endTime = Convert.ToDateTime("2019-08-31 11:00:00 pm");
             DateTime timeNow = startTime;
 
             while (timeNow <= endTime)
             {
-                decimal rate = (timeNow.Hour >= 9 && timeNow.Hour <= 10 && timeNow.Minute<=59 && timeNow.Second <=59) ? 20 : 30;
+                decimal rate = (timeNow.Hour >= 9 && timeNow.Hour <= 22 && timeNow.Minute<=59 && timeNow.Second <=59) ? 20 : 30;
                 bill = bill + rate;
-                Console.WriteLine("{0:HH:mm}, rate: ${1:#,0.00}, bill: ${2:#,0.00}", timeNow, rate, bill);
+
                 timeNow = timeNow.AddSeconds(20);
+                Console.WriteLine("{0:HH:mm:ss tt}, rate: {1:#,0.00}, bill: {2:#,0.00}", new DateTime(timeNow.Ticks).ToString("hh:mm:ss tt"), rate, bill);
             }
 
-            Console.WriteLine("Bill: {0:HH:mm:ss tt} to {1:HH:mm::ss tt}, {2:#,0} mins, ${3:#,0.00}", startTime, endTime, (endTime - startTime).TotalMinutes, bill);
+            Console.WriteLine("Bill: {0:HH:mm:ss tt} to {1:HH:mm:ss tt}, {2:#,0} mins, {3:#,0.00} taka", startTime, new DateTime(endTime.Ticks).ToString("hh:mm:ss tt"), (endTime - startTime).TotalMinutes, bill/100);
 
             Console.ReadLine();
         }
